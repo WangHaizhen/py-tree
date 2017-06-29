4223629d64a7e1cbf5d9e205c6466f8d46a393ed
wangcode <wanghaizhen1994@163.com>
2017-06-29 21:52:34 +0800

    updata IP process

--- 1_1.py
+++ 1_1.py
@@ -1,17 +1,12 @@
-#coding = utf8
-i = 0
-while( i < 4 ):
+#coding=utf8
+while True:
 	flag = 0 
 	s=input('Please enter string:')
-	x = s.split(' ')  
+	x = s.split()  
 	for j in range(4):
 		if int(x[j]) < 0 or int(x[j]) > 255:
 			print('[Error]: cannot compose IP address')
 			flag = 1
 			break
-			j = j + 1
-	y = '.'.join(x)  
-	#print(x)
 	if flag == 0:
-		print(y)
-	i = i + 1
+		print('.'.join(x))
