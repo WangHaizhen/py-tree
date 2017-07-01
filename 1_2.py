ecf56a45b29310e64f9dc1f64c7ceab093480145
wangcode <wanghaizhen1994@163.com>
2017-07-01 21:29:55 +0800

    Update URL

--- 1_2.py
+++ 1_2.py
@@ -1,12 +1,17 @@
 #coding=utf8
+__author__ = 'WangHaizhen'
+#import 1_1
 while True:
 	flag = 0 
 	s=input('Please enter string:')
 	x = s.split()  
-	for xx in x[1:4]:
+	Ip=x[1:5]
+	for xx in Ip:
 		if int(xx) < 0 or int(xx) > 255:
 			print('[Error]: cannot compose IP address')
 			flag = 1
 			break
+	y='.'.join(Ip)
 	if flag == 0:
-		print('.'.join(x))
+		print(x[0] + '://' + y  + ':' + x[5] + '/' + x[6])
+
