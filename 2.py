b5750ac1f8b423d26fe354bc5af5411583b50d0d
wangcode <wanghaizhen1994@163.com>
2017-07-04 20:55:57 +0800

    添加2.py(列表解析的双重循环)

--- 2.py
+++ 2.py
@@ -3,6 +3,4 @@
 while True:
 	A = input('Please input the first list:')
 	B = input('Please input the first list:')
-	L=[]
-	[L.append(s1[a]+s2[b]) for a in A for b in B]
-	print(L)
+	print( [int(a)+int(b) for a in A.split() for b in B.split()] )
