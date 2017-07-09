dcf6704f97d41bf42e8ea4ba1d0761e12b4eafae
wangcode <wanghaizhen1994@163.com>
2017-07-09 15:33:54 +0800

    更新了文件w_2_2.py,新增了文件w_2_3.py

--- w_2_2.py
+++ w_2_2.py
@@ -4,5 +4,6 @@ __author__ = 'WangHai zhen'
 
 for i in range(1, 10):
 	for j in range(1, i+1):
-		print('%dx%d=%-2d' % (i,j,i*j), end = '  ')
-	print(' ')
+		print('%dx%d=%-2d' % (i,j,i*j), end = '\t')
+	print()
+
