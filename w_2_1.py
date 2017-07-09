d6b5dcf11f8cebe17b04c21e14bea555cc142e10
wangcode <wanghaizhen1994@163.com>
2017-07-09 19:29:29 +0800

    更新了文件w_1_1.py-->w_1_3.py

--- w_2_1.py
+++ w_2_1.py
@@ -5,8 +5,8 @@ __author__ = 'WangHai zhen'
 while True:
 	A = input('Please input the first list:')
 	B = input('Please input the first list:')
-	for a in A.split():
-		for b in B.split():
+	for i,a in enumerate(A.split()):
+		for j,b in enumerate(B.split()):
 			print("列表 A 的第 %d 个元素和列表 B 的第 %d 个元素相加得 %d " 
-			 % ((A.split()).index(a) + 1, (B.split()).index(b) + 1, int(a)+int(b)) )
+			 % (i+1, j+1, int(a)+int(b)) )
 
