59a83f94b26c7fe0dc958bfe3ac768193d694094
wangcode <wanghaizhen1994@163.com>
2017-07-18 17:23:33 +0800

    用map+lambda重新编写了文件w_1_4.py

--- w_1_4.py
+++ w_1_4.py
@@ -5,7 +5,6 @@ __author__ = 'WangHai zhen'
 
 import base64
 s1='XDRjXDY5XDY2XDY1XDIwXDY5XDczXDIwXDczXDY4XDZmXDcyXDc0XDJjXDIwXDQ5XDIwXDc1XDczXDY1XDIwXDcwXDc5XDc0XDY4XDZmXDZlXDJlXGFcNGNcNjlcNjZcNjVcMjBcNjlcNzNcMjBcNjJcNjVcNjFcNzVcNzRcNjlcNjZcNzVcNmNcMmNcMjBcNDlcMjBcNmNcNmZcNzZcNjVcMjBcNjJcNjFcNmZcNjJcNjFcNmZcMmU='
-s2 = base64.b64decode(s1).decode('utf-8').split('\\')
-List = [int(i,16)  for i in s2  if i != '']
-for i in List:
-	print(chr(i),end='')
+L=map(lambda x: chr(x),[int(i,16)  for i in base64.b64decode(s1).decode('utf-8').split('\\')  if i != ''])
+print(''.join(L))
+	
